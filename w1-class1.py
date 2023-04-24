4581e2a4897199e66c7c99e81aee45f39b8199d5
Naeem Memon <naeem@unit55.io>
2023-04-24 15:42:19 +0400

    my test commit

--- w1-class1.py
+++ w1-class1.py
@@ -1,4 +1,6 @@
 print("This is my first Python comment")
 fname = input("Please Enter Your Name: ")
-print(f"Hello, Mr. {fname}. Nice to Meet You! ")
+x = fname.capitalize()
+print(f"Hello, Mr. {x}. Nice to Meet You! ")
+
 
